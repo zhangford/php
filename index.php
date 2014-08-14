1895dd889497416450f4b1dd5636df6c078ff072
zhangford <zhang.ford@gmail.com>
2014-08-14 09:28:54 +0800

    完成第二章

--- index.php
+++ index.php
@@ -6,6 +6,19 @@
  * Date: 14-8-13
  * Time: 下午1:16
  */
+?>
+<!DOCTYPE html>
+<html lang="zh">
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="css/default.css" />
+</head>
+<body>
+<?php
 include('./tips.html');
 include('./preface.php');
 include('./chapter1.php');
+include('./chapter2.php');
+?>
+</body>
+</html>
