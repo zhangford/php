01d754f5a9f1dfe5dec13f8881666cf4e83a00c0
zhangford <zhang.ford@gmail.com>
2014-09-01 16:15:38 +0800

    结束第九章 这一章内容很多，不常用，讲得也不够清楚。用到时再说。这本书就看这一遍就够了，没什么再看一次的必要。

---
+++ chapter10.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ford
+ * Date: 14-9-1
+ * Time: 下午4:12
+ */
+?>
+<header><h1>使用PEAR</h1></header>
+<section>
+	<article>
+		<h2></h2>
+		<h3></h3>
+		<p></p>
+	</article>
+
+</section>
+<footer>
+</footer>
